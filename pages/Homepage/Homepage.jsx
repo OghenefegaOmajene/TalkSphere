0ec3915900b5d4acecd15cbc3071531acc198cbc
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-04-28 23:11:05 +0100

    Working on hero section

--- pages/Homepage/Homepage.jsx
+++ pages/Homepage/Homepage.jsx
@@ -18,6 +18,13 @@ const Homepage = () => {
 
         </div>
 
+        <div className="heroSection">
+            <h1>Download Talksphere</h1>
+            <h2>Connect with anyone around the globe, Talk within your sphere.</h2>
+            <h3>Seamless cross-platform collaboration across your devices.</h3>
+            <p>By installing Talksphere, you agree to our <a href="">Terms & Privacy Policy.</a></p>
+            <button className='download'>Download</button>
+        </div>
     </div>
   )
 }
